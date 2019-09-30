858ed31c5fda2e3186f2dbb2c4b844d5fc2c0ae9
erka <erik.westerberg@hotmail.se>
2019-09-30 20:24:13 +0200

    installs socket.io depedenci

--- src/Store.js
+++ src/Store.js
@@ -1,46 +1,46 @@
 import React from "react";
+import io from "socket.io-client";
 
 export const CTX = React.createContext();
 
 const initState = {
   general: [
-      {from: "Pedro", msg: "yoo"},
-      {from: "Seth", msg: "Hollywood"},
-      {from: "Erka", msg: "Life"}
+    { from: "Pedro", msg: "yoo" },
+    { from: "Seth", msg: "Hollywood" },
+    { from: "Erka", msg: "Life" }
   ],
-  topic2 : [
-  {from: "Code", msg: "Yeeees"},
-  {from: "ContextApi", msg: "yiiiihaaa"},
-  {from: "Christmas", msg: "Maybe"}
+  topic2: [
+    { from: "Code", msg: "Yeeees" },
+    { from: "ContextApi", msg: "yiiiihaaa" },
+    { from: "Christmas", msg: "Maybe" }
   ]
-}
+};
 const reducer = (state, action) => {
-  const { from, msg, topic } = action.payload
-    switch(action.type) {
-       case "RECIVE_MESSAGE":
-         return {
-            ...state, 
-            [topic] : [
-              ...state[topic],
-                {
-                  from,
-                  msg
-                }
-            ]
-         }
-         default:
-            return state
-    }
-}
+  const { from, msg, topic } = action.payload;
+  switch (action.type) {
+    case "RECIVE_MESSAGE":
+      return {
+        ...state,
+        [topic]: [
+          ...state[topic],
+          {
+            from,
+            msg
+          }
+        ]
+      };
+    default:
+      return state;
+  }
+};
 
+let socket;
 
 export default function Store(props) {
+  if (!socket) {
+    socket = io(":3001");
+  }
+  const reducerHook = React.useReducer(reducer, initState);
 
-  const reducerHook = React.useReducer(reducer, initState)
-
-  return (
-    <CTX.Provider value={reducerHook}>
-      {props.children}
-    </CTX.Provider>
-  )
+  return <CTX.Provider value={reducerHook}>{props.children}</CTX.Provider>;
 }
